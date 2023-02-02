ebe133ee3972ff9aa4895cf35a9465b923ed6708
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-02-02 19:59:15 +0100

    adds 'round doesn't count' to the DRAW result output

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ const playGame = (playerInput) => {
     
     } else if  ((compChoice === "rock" && playerChoice === "rock") || 
                 (compChoice === "paper" && playerChoice === "paper") || (compChoice === "scissors" && playerChoice === "scissors")) {
-        roundWinner.innerHTML = "Draw..";
+        roundWinner.innerHTML = "Draw. Round doesn't count.";
     }
 
     //this statement has to be embedded here, otherwise the rounds-count geht eine zahl drüber bevor GAME OVER kommt !!!
@@ -81,7 +81,50 @@ const playGame = (playerInput) => {
 
 
 
+/* fragen sven:
 
+- onclick bei btns - haben value als string drinne (statt value des buttons abzurufen nachher) - wie geht das? ist das dann quasi wie value???
+warum kann ich nicht über form die calue btn abrufen? oder über class X bei btn und dann deren value..
+*/
+
+//warum bei if in klammern muss === stehen und es mit 0 nicht funktioniert
+//deswegen habe ich imer verloren und der count ging nicht????
+
+
+/* const x = document.querySelector(".x")
+why can't i get the vakue of btns thru either class given to btns/ or id to the parent-form??? */
+/* rockBtn.addEventListener('click', calculateGameOutcome);
+paperBtn.addEventListener('click', calculateGameOutcome);
+scissorsBtn.addEventListener('click', calculateGameOutcome); */
+
+
+
+/* const fiveRounds = document.getElementById("fiveRounds");
+const tenRounds = document.getElementById("tenRounds");
+const fifteenRounds = document.getElementById("twentyRounds");
+const twentyRounds = document.getElementById("twentyRounds"); */
+
+/*
+// Rock- Paper - Scissors-Btns
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn"); */
+
+
+
+// sinnloses probieren???
+/*  let pointsCalculationComp;
+  let pointsCalculationPlayer; */
+/*  if (howManyRounds === 5) {
+    pointsCalculationComp = Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp);
+    pointsCalculationPlayer = Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer);
+  } else if (howManyRounds === 15) {
+    pointsCalculationComp = Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp);
+    pointsCalculationPlayer = Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer);
+  } else {
+    pointsCalculationComp = Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp) + Number(pointForComp);
+    pointsCalculationPlayer = Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer) + Number(pointForPlayer);
+  } */
 
 
 
